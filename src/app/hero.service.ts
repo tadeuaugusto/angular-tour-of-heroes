7558a8a8cfff18d0060b87069cd820c47474c861
tadeuaugusto <tadeuaugusto@gmail.com>
2019-02-26 19:53:38 -0300

    Get heroes and get hero by id with HttpClient

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,24 +1,61 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
-import { HEROES } from "./mock-heroes";
 import { Observable, of } from "rxjs";
 import { MessageService } from "./message.service";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { catchError, map, tap } from "rxjs/operators";
+
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';
 
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+
+  /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
 
     this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl)
+        .pipe(
+            tap(_ => this.log('HeroService: fetched heroes')),
+            catchError(this.handleError('getHeroes', []))
+        );
   }
 
   getHero(id: number): Observable<Hero> {
 
+    const url = `${this.heroesUrl}/${id}`;
+
     this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`HeroService: fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  /**
+  * Handle Http operation that failed.
+  * Let the app continue.
+  * @param operation - name of the operation that failed
+  * @param result - optional value to return as the observable result
+  */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    }
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
